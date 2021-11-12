9202242c86358505c4901401d2320b1c91d33acd
simonemazzacano <simone.mazzacano.stud@ispascalcomandini.it>
2021-11-12 08:34:33 +0100

    Classe libro completa

--- progBiblioteca/Libro.cs
+++ progBiblioteca/Libro.cs
@@ -8,19 +8,35 @@ namespace progBiblioteca
 {
     class Libro
     {
-           
+        public Libro(string autore, string titolo, int annoPubblicazione, string editore, int numPagine)
+        {
+            Autore = autore;
+            Titolo = titolo;
+            AnnoPubblicazione = annoPubblicazione;
+            Editore = editore;
+            NumPagine = numPagine;
+        }
+
         public string Autore { get; set; }
         public string Titolo { get; set; }
         public int AnnoPubblicazione { get; set; }
         public string Editore { get; set; }
         public int NumPagine { get; set; }
 
+        public int ReadingTime()
+        {
+            if (NumPagine > 200)
+                return 3;
+            else if (NumPagine > 100)
+                return 2;
+            else
+                return 1;
+        }
+
         public override string ToString()
         {
-            //
             return $"Autore: {Autore}\nTitolo: {Titolo}\nAnno pubblicazione: {AnnoPubblicazione}\n Editore: {Editore}\nNumero pagine: {NumPagine}";
         }
-        
     }
 }
 
