3c4b0d818928f56e826892825743673a64e63edd
simonemazzacano <simone.mazzacano.stud@ispascalcomandini.it>
2021-11-12 08:31:16 +0100

    Proprietà classe Libro

---
+++ progBiblioteca/Libro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progBiblioteca
+{
+    class Libro
+    {
+           
+        public string Autore { get; set; }
+        public string Titolo { get; set; }
+        public int AnnoPubblicazione { get; set; }
+        public string Editore { get; set; }
+        public int NumPagine { get; set; }
+
+        public override string ToString()
+        {
+            //
+            return $"Autore: {Autore}\nTitolo: {Titolo}\nAnno pubblicazione: {AnnoPubblicazione}\n Editore: {Editore}\nNumero pagine: {NumPagine}";
+        }
+        
+    }
+}
+
