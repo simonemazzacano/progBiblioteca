685e8f5ce480e3ee5391dbdae288a35512e879c2
simonemazzacano <simone.mazzacano.stud@ispascalcomandini.it>
2021-11-12 08:52:10 +0100

    Metodi e costruttore biblioteca

--- progBiblioteca/Biblioteca.cs
+++ progBiblioteca/Biblioteca.cs
@@ -8,6 +8,9 @@ namespace progBiblioteca
 {
     class Biblioteca
     {
+
+        public Biblioteca(string nome, string indirizzo, DateTime apertura, DateTime chiusura): this(nome, indirizzo, apertura, chiusura, new List<Libro>()) { }
+        
         public Biblioteca(string nome, string indirizzo, DateTime apertura, DateTime chiusura, List<Libro> libri)
         {
             Nome = nome;
@@ -22,5 +25,37 @@ namespace progBiblioteca
         public DateTime Apertura { get; set; }
         public DateTime Chiusura { get; set; }  
         public List<Libro> Libri { get; set; }
+
+        public void AggiungiLibro(Libro l)
+        {
+            Libri.Add(l);
+        }
+
+        public Libro CercaLibroTitolo(string titolo)
+        {
+            foreach (Libro libro in Libri)
+            {
+                if (libro.Titolo == titolo) return libro;
+            }
+
+            return null;
+        }
+
+        public List<Libro> CercaLibriAutore(string autore)
+        {
+            List<Libro> trovati = new List<Libro>();
+            foreach (Libro libro in Libri)
+            {
+                if (libro.Autore == autore)
+                    trovati.Add(libro);
+            }
+
+            return trovati;
+        }
+
+        public int NumeroLibri()
+        {
+            return Libri.Count;
+        }
     }
 }
