9e2464c83e3585fe29d2edc74d4d5ceeac1995c4
simonemazzacano <simone.mazzacano.stud@ispascalcomandini.it>
2021-11-12 08:42:16 +0100

    Proprietà Biblioteca

---
+++ progBiblioteca/Biblioteca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progBiblioteca
+{
+    class Biblioteca
+    {
+        public Biblioteca(string nome, string indirizzo, DateTime apertura, DateTime chiusura, List<Libro> libri)
+        {
+            Nome = nome;
+            Indirizzo = indirizzo;
+            Apertura = apertura;
+            Chiusura = chiusura;
+            Libri = libri;
+        }
+
+        public string Nome { get; set; }
+        public string Indirizzo { get; set; }
+        public DateTime Apertura { get; set; }
+        public DateTime Chiusura { get; set; }  
+        public List<Libro> Libri { get; set; }
+    }
+}
